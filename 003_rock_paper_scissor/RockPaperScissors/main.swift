563e0637670221ab10d4877e0b034b2acca340e6
Ariana Nagumo <arinagumo@gmail.com>
2025-01-24 14:20:07 -0600

    feat(rock_paper_scissor): add main function definition

---
+++ 003_rock_paper_scissor/RockPaperScissors/main.swift
@@ -0,0 +1,26 @@
+//
+//  main.swift
+//  RockPaperScissors
+//
+//  Created by Ariana Rodríguez on 23/01/25.
+//
+
+import Foundation
+
+// MARK: Main function that excutes the game
+/// Implementation of the classic game "Rock, Paper or Scissor" where the user can play with the computer.
+/// 1. Display options: rock, paper, scissor (read option, get enum value and save user option)
+/// 2. Computer turns (create a random number generator, get the enum value, save option in a new variable)
+/// 3. Evaluate both answers based on the game rules (evaluate both options and save the result in a new variable)
+/// 4. Display results (display formatted string and the options game again)
+/// 5. If user select exit, close the game (break the bucle and display a final message)
+///
+/// #Implementation
+/// - The program should use a bucle to be able to play until the user exit
+/// - Handle invalid outoup with clear messages
+/// - Uses a random number generator to select the computer answer
+func main() {
+    
+}
+
+main()
