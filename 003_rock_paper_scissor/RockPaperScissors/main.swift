42aa1cc90d5406689c531cc1a942abad91066a9b
Ariana Nagumo <arinagumo@gmail.com>
2025-01-24 17:01:37 -0600

    feat(rock_paper_scissor): add display options function

--- 003_rock_paper_scissor/RockPaperScissors/main.swift
+++ 003_rock_paper_scissor/RockPaperScissors/main.swift
@@ -7,6 +7,22 @@
 
 import Foundation
 
+// MARK: Block functions
+enum Option: String, CaseIterable {
+    case Rock
+    case Paper
+    case Scissors
+    case Exit
+}
+
+func displayOptions() -> String {
+    var optionsMessage = "Select an option\n"
+    for (index, option) in Option.allCases.enumerated() {
+        optionsMessage = optionsMessage + "\(index) - \(option.rawValue)\n"
+    }
+    return optionsMessage
+}
+
 // MARK: Main function that excutes the game
 /// Implementation of the classic game "Rock, Paper or Scissor" where the user can play with the computer.
 /// 1. Display options: rock, paper, scissor (read option, get enum value and save user option)
@@ -17,10 +33,10 @@ import Foundation
 ///
 /// #Implementation
 /// - The program should use a bucle to be able to play until the user exit
-/// - Handle invalid outoup with clear messages
+/// - Handle invalid output with clear messages
 /// - Uses a random number generator to select the computer answer
 func main() {
-    
+    print(displayOptions())
 }
 
 main()
