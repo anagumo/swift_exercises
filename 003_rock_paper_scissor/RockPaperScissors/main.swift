181c898d0b2cab0272600391ef0f22f43351d621
Ariana Nagumo <arinagumo@gmail.com>
2025-01-24 18:09:29 -0600

    feat(rock_paper_scissor): add function to handle user input

--- 003_rock_paper_scissor/RockPaperScissors/main.swift
+++ 003_rock_paper_scissor/RockPaperScissors/main.swift
@@ -7,14 +7,30 @@
 
 import Foundation
 
-// MARK: Block functions
 enum Option: String, CaseIterable {
     case Rock
     case Paper
     case Scissors
-    case Exit
+    case Quit
+    case Invalid
+    
+    init?(input: Int) {
+        switch input {
+        case 0:
+            self = .Rock
+        case 1:
+            self = .Paper
+        case 2:
+            self = .Scissors
+        case 3:
+            self = .Quit
+        default:
+            self = .Invalid
+        }
+    }
 }
 
+// MARK: Block functions
 /// Display options that can be selected to play with the computer
 /// - returns: an string (`String`) that represent a list of options
 /// - note: the function do not save the option selected by the user
@@ -34,7 +50,7 @@ enum Option: String, CaseIterable {
 ///0 - Rock
 ///1 - Paper
 ///2 - Scissors
-///3 - Exit
+///3 - Quit
 ///```
 
 func displayOptions() -> String {
@@ -45,9 +61,35 @@ func displayOptions() -> String {
     return optionsMessage
 }
 
-// MARK: Main function that excutes the game
+/// Handle user input
+/// - parameters:
+///     - inputText:  the text of type  (`String`) that user writes as input
+/// - returns: an enum (`Option`) case that represent a selected option
+/// - note: the function do not save the option selected by the user
+///
+/// # Implementation:
+/// To handle the input create an enum (`Option`) using the rawValue
+///
+/// Usage:
+///```swift
+///let userOption = readUserOption("1")
+///print(userOption)
+///```
+///
+///Output:
+///```
+///Optional(RockPaperScissors.Option.Paper)
+///```
+func readUserInput(_ inputText: String) -> Option? {
+    guard let userOption = Option(input: Int(inputText) ?? -1) else {
+        return nil
+    }
+    
+    return userOption
+}
+
+// MARK: Main function
 /// Implementation of the classic game "Rock, Paper or Scissor" where the user can play with the computer.
-/// 1. Display options: rock, paper, scissor (read option, get enum value and save user option)
 /// 2. Computer turns (create a random number generator, get the enum value, save option in a new variable)
 /// 3. Evaluate both answers based on the game rules (evaluate both options and save the result in a new variable)
 /// 4. Display results (display formatted string and the options game again)
@@ -57,8 +99,6 @@ func displayOptions() -> String {
 /// - The program should use a bucle to be able to play until the user exit
 /// - Handle invalid output with clear messages
 /// - Uses a random number generator to select the computer answer
-func main() {
-    print(displayOptions())
-}
+func main() {}
 
 main()
